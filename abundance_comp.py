1a4a0a30e7795ebf744540b178a410284cdf0c9e
katiesch <kschlesinger@gmail.com>
2016-05-08 16:09:16 +1000

    added in hover with sobject_id display

--- abundance_comp.py
+++ abundance_comp.py
@@ -10,32 +10,37 @@ in your browser.
 import numpy as np
 
 from bokeh.plotting import Figure
-from bokeh.models import ColumnDataSource, HBox, VBoxForm
-from bokeh.models.widgets import Slider, TextInput,Select
+from bokeh.models import ColumnDataSource, HBox, VBoxForm,Range1d, HoverTool
+from bokeh.models.widgets import TextInput,Select
 from bokeh.io import curdoc
+
 
 # Set up data
 from astropy.table import Table
 abund=Table.read('/Users/kschles/Documents/GALAH/wg4output/wg4_04292016/sobject_iraf_k2.fits', format='fits')
 abund=Table.to_pandas(abund)
 
-source = ColumnDataSource(data=dict(x=abund.loc[0:100,'feh_cannon'], y=abund.loc[0:100,'alpha_fe_cannon']))
+source = ColumnDataSource(data=dict(x=abund.loc[0:100,'feh_cannon'], y=abund.loc[0:100,'alpha_fe_cannon'], sobject_id=abund.loc[0:100,'sobject_id']))
+
+hover = HoverTool(tooltips=[("sobject_id","@sobject_id")])
 
 # Set up plot
-plot = Figure(plot_height=400, plot_width=400, title="My Abundance Plot",
-              tools="crosshair,pan,reset,resize,save,box_zoom",
-              x_range=[-2,1.0], y_range=[-0.2,1.0])
+#plot = Figure(plot_height=400, plot_width=400, title="My Abundance Plot",
+#              tools="crosshair,pan,reset,resize,save,box_zoom")
+plot = Figure(plot_height=400, plot_width=400, title="My Abundance Plot",tools=[hover])
 plot.xaxis.axis_label = "[Fe/H]"
 plot.yaxis.axis_label = "[a/Fe]"
 
 plot.scatter('x', 'y', source=source, color='black')
 
 axis_map = {
-    "[Fe/H]": "feh_sme",
-    "[a/Fe]": "alpha_fe_sme",
+    "[Fe/H]": "feh_cannon",
+    "[a/Fe]": "alpha_fe_cannon",
     "Ca": "ca_abund_sme",
     "Mg": "mg_abund_sme",
 }
+
+
 
 # Set up widgets
 text = TextInput(title="title", value='my abundance plot')
@@ -55,8 +60,8 @@ def update_data(attrname, old, new):
     y_name = axis_map[select_y.value]
     plot.xaxis.axis_label = select_x.value
     plot.yaxis.axis_label = select_y.value
-
-    source.data = dict(x=df.loc[0:100,x_name], y=df.loc[0:100,y_name])
+    
+    source.data = dict(x=df.loc[0:100,x_name], y=df.loc[0:100,y_name],sobject_id=df.loc[0:100,'sobject_id'])
 
 for w in [select_x,select_y]:
     w.on_change('value', update_data)
